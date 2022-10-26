cec025309ba3e3da9d1df5d91f2e51cbdeb215c3
tmasjc <thomas.jcng@gmail.com>
2022-10-26 13:44:38 +0800

    Make func generate data in batches

--- generate_fake_data.R
+++ generate_fake_data.R
@@ -1,7 +1,7 @@
-generate_fake_data <- function() {
+generate_fake_data <- function(min = 1, max = 10) {
 
     # helper function to generate data
-    gen_ <- function(opts, prob = "", length = 500) {
+    gen_ <- function(opts, prob = "", length = max) {
         n <- length(opts)
         p <- if (all(prob == "")) rep(1 / n, n) else prob
         sample(
@@ -14,20 +14,19 @@ generate_fake_data <- function() {
 
     # parameters for fake data
     params <- list(
-        N   = 500,
         sex = c("Male", "Female"),
         src = c("Facebook Ads", "Google SEM", "Inbound Marketing")
     )
 
     # this is our fake data
     tibble::tibble(
-        ID     = 1:params$N,
+        ID     = ids::random_id(max),
         Sex    = gen_(params$sex, c(0.3, 0.7)),
         Date   = gen_(seq(Sys.Date() - 3, Sys.Date(), by = 1)),
         Source = gen_(params$src, c(0.2, 0.2, 0.2)),
-        CAC    = rnorm(n = params$N, mean = 150, sd = 50)
+        CAC    = rnorm(n = max, mean = 150, sd = 50)
     ) |>
-        dplyr::sample_n(round(runif(1, min = 400, max = 500))) |>
+        dplyr::sample_n(round(runif(1, min = min, max = max))) |>
         # convert to JSON convenient
         purrr::pmap(function(...) list(...)) |>
         purrr::map(function(x) list(fields = x))
